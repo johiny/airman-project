ca46f51da07c31bba8741982894f6c62f4e8b899
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-06 00:24:18 -0600

    modified bg color

--- src/components/Announce.jsx
+++ src/components/Announce.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Announce() {
   return (
-    <div className='bg-[#8a4af3] font-bold text-white flex items-center justify-center'>
+    <div className='bg-[#2A2550] font-bold text-white flex items-center justify-center'>
         <h2>Date prisa, ahora tiene un 50 % de descuento</h2>
     </div>
   )
