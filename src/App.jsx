7622e3d313c1a8b38680024467f58d5f01ff987a
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-07 13:26:13 -0600

    add component Categories.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Slider from "./components/Slider";
+import Categories from "./components/Categories";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Home/>
       <Navbar/>
       <Slider/>
+      <Categories/>
     </div>
   )
 }
