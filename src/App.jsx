45cbddc8c1261f94d9e8461d9c15156cbb83d33b
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-07 02:43:13 -0600

    add Slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
 
 function App() {
   return (
     <div>
       <Home/>
       <Navbar/>
+      <Slider/>
     </div>
   )
 }
