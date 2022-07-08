8d3f94aa9e1090b091693c27e1ec563922b6274f
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-07 19:48:51 -0600

    add Product and Products.jsx TopProductsApi.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
-import Slider from "./components/Slider";
-import Categories from "./components/Categories";
 
 function App() {
   return (
     <div>
       <Home/>
-      <Navbar/>
-      <Slider/>
-      <Categories/>
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Announce from '../components/Announce';
+import Navbar from "../components/Navbar";
+import Slider from "../components/Slider";
+import Categories from "../components/Categories";
+import Products from "../components/Products";
 
 function Home() {
-  return (
-    <div>
+  return <div>
         <Announce/>
+        <Navbar/>
+        <Slider/>
+        <Categories/>
+        <Products/>
     </div>
-  )
+  
 }
 
 export default Home
