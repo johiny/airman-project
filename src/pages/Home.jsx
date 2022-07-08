414a85692efca05f6e3d2ef96fb3195b34bc0aae
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-07 21:07:05 -0600

    add Newsletter.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
 import Products from "../components/Products";
+import Newsletter from '../components/Newsletter';
 
 function Home() {
   return <div>
@@ -12,6 +13,7 @@ function Home() {
         <Slider/>
         <Categories/>
         <Products/>
+        <Newsletter/>
     </div>
   
 }
