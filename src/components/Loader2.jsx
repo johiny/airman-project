3b1160eb2ae576477608172c7ddb9fba20f57198
Bephoros <bephorosmain@gmail.com>
2022-07-19 17:40:38 -0600

    Loader
    Loader para la pantalla de procesando transacción

---
+++ src/components/Loader2.jsx
@@ -0,0 +1,9 @@
+import './Loader2.css'
+
+const Loader2 = () => {
+    return ( 
+        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+     );
+}
+ 
+export default Loader2;
