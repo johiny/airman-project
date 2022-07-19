df31b1103662425812121135a75ca3da9d6c1f7f
johiny <johiny7@live.com>
2022-07-18 22:03:18 -0500

    funcion de agregado al carrito avanzada agregada

--- src/Redux/Slices/cartSlice.js
+++ src/Redux/Slices/cartSlice.js
@@ -5,13 +5,29 @@ const initialState = {
     status: "idle",
     error: null
 }
+
+const advanceAdd = (products, newProduct) => {
+    
+    for ( let i = 0; i < products.length; i++)
+    {
+        if(products[i].id == newProduct.id)
+        {
+            let productsCopy = Array.from(products)
+            productsCopy[i].quantity += 1 
+            return productsCopy
+        }  
+    }
+
+    return [...products, {...newProduct, quantity : 1}]
+}
+
 const Cart = createSlice({
     name: "cart",
     initialState,
     reducers: {
         add(state, action){
 
-            state.data = [...state.data, action.payload]
+            state.data = advanceAdd(state.data, action.payload)
         },
         remove(state, action){
             state.data = state.date.filter((product) => product.id != action.payload)
