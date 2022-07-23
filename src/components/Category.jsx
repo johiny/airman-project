b6b5b6dc915546e99bfc39145fbc1648ef97b882
johiny <johiny7@live.com>
2022-07-22 20:23:11 -0500

    nuevas categorias funcionando!

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,14 +1,14 @@
-
 import React from 'react';
+import {useNavigate} from "react-router-dom"
+function Category({category}) {
+    const navigate = useNavigate()
 
-function Category({item}) {
   return <div className='flex-1 m-2 shadow-lg rounded-md overflow-hidden relative'>
-            <img src={item.src}  alt='category_image' className='w-[100%]' />
+            <img src={category.src}  alt='category_image' className='w-[100%]' />
             <div className='flex absolute w-[100%] h-[100%] left-0 top-0 items-center justify-center flex-col'>
-                <h2 className='text-white font-black text-[30px]'>{item.title}</h2>
-                <button className='btn'>Ver más</button>
-            </div>
-            
+                <h2 className='text-white font-black text-[30px]'>{category.title}</h2>
+                <button className='btn' onClick={() => navigate(`/search/${category.query}`)}>Ver más</button>
+            </div>           
         </div>;
   
 }
