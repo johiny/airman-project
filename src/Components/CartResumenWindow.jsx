f2b18f06a8a2778a12d76c29b4789673a3d56031
Erik Hernandez <erikestuardo_161@hotmail.com>
2022-07-24 00:45:17 -0600

    espacio en resumen

--- src/Components/CartResumenWindow.jsx
+++ src/Components/CartResumenWindow.jsx
@@ -7,7 +7,7 @@ const CartResumenWindow = () => {
     const discount = useSelector(selectStoreDiscount)
     const shipmentprice = useSelector(selectShippingPrice)
   return (
-    <div className=' p-5 flex-[0.4] w-auto h-[40vh] border-2 border-[#2A2550] rounded-md shadow-lg flex flex-col  items-center mobile:mb-6'>
+    <div className=' p-5 flex-[0.4] w-auto h-[25vh] border-2 border-[#2A2550] rounded-md shadow-lg flex flex-col  items-center mobile:mb-6'>
         <h1 className='text-[2rem]'>Resumen</h1>
         <div className={resumenStyle}>
             <p>Subtotal</p>
@@ -27,7 +27,7 @@ const CartResumenWindow = () => {
         </div>
         {total == 0? 
         <div className={resumenStyle + ` text-2xl font-bold`}>
-            <p>Aun no tienes nada en tu carrito<br/> porque no te animas y agregas algo!</p>
+            <p></p>
         </div> : null }
     </div>
   )
