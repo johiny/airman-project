71a6753ab81d78ee97b6cb5dfc2022e5a62b09f5
Johiny <johiny7@live.com>
2023-02-04 15:40:35 -0500

    fix on weird import

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import './index.css'
 import WebRoutes from './Components/WebRoutes';
 import {Route, BrowserRouter} from "react-router-dom"
-import CategoryPage from "./Pages/CategoryPage";
 import Home from "./Pages/Home";
 import ProductPage from "./Pages/ProductPage";
-import ProductPage2 from './views/ProductPage';
 import Cart from "./Pages/Cart";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Resultados from './Pages/Resultados'
 import DataForm from './views/DataForm'
-import MainLayout from './components/MainLayout';
+import MainLayout from './Components/MainLayout';
 import Confirmation from './Pages/Confirmation';
 import Processing from './Pages/Processing';
 function App() {
@@ -19,7 +17,6 @@ function App() {
     <WebRoutes>
         <Route path='/' element={<MainLayout/>}>
             <Route path='/' element={<Home/>} />
-            <Route path="categories" element={<CategoryPage/>} />
             <Route path="search" element={<Resultados/>}>
               <Route path=":query" element={<Resultados/>}/>
             </Route>
